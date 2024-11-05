08ea222da2893e6400efe980dafa9a16c07379a9
AJ1732 <ejemeniboi@gmail.com>
2024-11-05 15:22:26 +0100

    chore: build stats section layout

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -2,10 +2,10 @@ import { StatsSection, TableSection } from "@/components";
 
 const UserPage = () => {
   return (
-    <div>
+    <>
       <StatsSection />
       <TableSection />
-    </div>
+    </>
   );
 };
 export default UserPage;
--- src/components/pages/users/StatsSection/index.tsx
+++ src/components/pages/users/StatsSection/index.tsx
@@ -1,4 +1,16 @@
+import styles from "./statSection.module.scss";
+
 const StatsSection = () => {
-  return <section>StatsSection</section>;
+  return (
+    <section className={styles["statsContainer"]}>
+      {[1, 2, 3, 4].map((item) => (
+        <div key={item}>
+          <div></div>
+          <h4>title</h4>
+          <p>1000</p>
+        </div>
+      ))}
+    </section>
+  );
 };
 export default StatsSection;
