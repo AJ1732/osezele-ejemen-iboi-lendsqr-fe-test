fe6200cdc475e6a847597828b5063a10703da0e3
AJ1732 <ejemeniboi@gmail.com>
2024-11-10 19:34:05 +0100

    style: update style in badge component

--- src/components/ui/Badge/index.tsx
+++ src/components/ui/Badge/index.tsx
@@ -9,7 +9,7 @@ const Badge: React.FC<BadgeProps> = ({ label }) => {
   const badgeClass = clsx(styles.badge, {
     [styles["badge--inactive"]]: label.toLowerCase() === "inactive",
     [styles["badge--pending"]]: label.toLowerCase() === "pending",
-    [styles["badge--blacklisted"]]: label.toLowerCase() === "blacklist",
+    [styles["badge--blacklisted"]]: label.toLowerCase() === "blacklisted",
     [styles["badge--active"]]: label.toLowerCase() === "active",
   });
 
